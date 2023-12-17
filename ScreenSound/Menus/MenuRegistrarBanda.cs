42d8c44061e007a88d5779eb131bf6f4a72e11e9
Vinícius Vilar <viniciusvillarma7x@gmail.com>
2023-12-17 14:09:02 -0300

    Inicio do projeto ScreenSound

---
+++ ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -0,0 +1,31 @@
+﻿using OpenAI_API;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarBanda : Menu
+{
+    public async override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+        ExibirTituloDaOpcao("Registro das bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Banda banda = new Banda(nomeDaBanda);
+        bandasRegistradas.Add(nomeDaBanda, banda);
+
+        var cliente = new OpenAIAPI("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");
+
+        var chat = cliente.Chat.CreateConversation();
+
+        chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em 1 parágrafo. Adote um estilo informal.");
+
+        string resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        banda.Resumo = resposta;
+
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
